bb67608d5749004af7988e57cc9e0a7c0ed626b8
Tarook <Tarook.Zinou@gmail.com>
2023-02-19 04:07:01 +0100

    Basic ladder done
    eh itsok i guess :skull:

--- Assets/script/Ladder.cs
+++ Assets/script/Ladder.cs
@@ -1,18 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Ladder : MonoBehaviour
 {
-    // Start is called before the first frame update
+    GameObject player;
+    public bool isClimbing = false;
+    public bool canGoUp = false;
+    public int ladderHeight = 1; // height in blocks
+    [SerializeField] private Vector3 finalPos;
+
+    public Sprite ladderSprite;
+
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        finalPos = new Vector3(transform.position.x, transform.position.y + ladderHeight + .5f, transform.position.z);
+
+        for(int i = 0; i < ladderHeight; i++)
+        {
+            GameObject newLadder = new GameObject("LadderPart");
+            newLadder.AddComponent(typeof(SpriteRenderer));
+            newLadder.GetComponent<SpriteRenderer>().sprite = ladderSprite;
+            newLadder.transform.parent = this.transform;
+            newLadder.transform.localPosition = new Vector3(0f, i, 0f);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider col)
     {
-        
+        if(col.tag == "Player")
+        {
+            if ((bool)Variables.Object(player).Get("canMove"))
+            {
+                Variables.Object(player).Set("canMove", false);
+                player.GetComponent<CapsuleCollider>().enabled = false;
+            }
+
+            isClimbing = true;
+            StartCoroutine(delayClimb());
+        }
+    }
+    
+    void OnTriggerStay(Collider col)
+    {
+        player.transform.position = Vector3.Lerp(player.transform.position, new Vector3(this.transform.position.x, player.transform.position.y, player.transform.position.z), .1f);
+    }
+
+    private void Update()
+    {
+        if (isClimbing)
+        {
+            if (canGoUp)
+            {
+                player.transform.position = Vector3.Lerp(player.transform.position, new Vector3(this.transform.position.x, finalPos.y, player.transform.position.z), .0080f);
+                if (player.transform.position.y - finalPos.y < .1f && player.transform.position.y - finalPos.y > -.1f)
+                {
+                    Debug.Log("Finished climbing");
+                    Variables.Object(player).Set("canMove", true);
+                    player.GetComponent<CapsuleCollider>().enabled = true;
+                    isClimbing = false;
+                    canGoUp = false;
+                }
+            }
+        }
+    }
+
+    IEnumerator delayClimb()
+    {
+        yield return new WaitForSeconds(.5f);
+        canGoUp = true;
     }
 }
