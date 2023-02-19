9ae0603402eb0a80462828093f1223c0539134af
Tarook <Tarook.Zinou@gmail.com>
2023-02-19 05:00:05 +0100

    Clicking on player gives small speed boost
    Might be used in some puzzles idk keep it in mind

--- Assets/script/PlayerScript.cs
+++ Assets/script/PlayerScript.cs
@@ -1,8 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerScript : MonoBehaviour
 {
-    
+    float baseSpeed;
+    private void Start()
+    {
+        baseSpeed = (float)Variables.Object(this).Get("speed");
+    }
+    private void Update()
+    {
+        if(Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                if(hit.collider.tag == "Player"){
+                    StopAllCoroutines();
+                    StartCoroutine(speedBoost());
+                }
+            }
+        }
+    }
+
+    IEnumerator speedBoost()
+    {
+        Variables.Object(this).Set("speed", baseSpeed + 1);
+        yield return new WaitForSeconds(.25f);
+        Variables.Object(this).Set("speed", baseSpeed);
+    }
 }
